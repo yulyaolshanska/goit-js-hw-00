ed983d0f06ef6db0b1ba44650eb355d3e53a15ce
DenysTutov <tutov1987@gmail.com>
2022-04-28 08:59:28 +0300

    Add 3rd task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+const formEl = document.querySelector('.feedback-form');
 
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener('input', onTextInput);
+formEl.addEventListener('submit', onFormSubmit);
+
+getStorageData();
+
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+
+function onTextInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  console.log('Email:', savedData.email);
+  console.log('Message:', savedData.message);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function getStorageData() {
+  const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (savedData) {
+    formEl.elements.email.value = savedData.email;
+    formEl.elements.message.value = savedData.message;
+  }
+}
